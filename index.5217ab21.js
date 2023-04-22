a945fd43200ffb45f3db6794829257aea1d7803e
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-04-22 16:13:13 +0000

    Deploying to gh-pages from @ irashovdra/simply-chocolate@e284d2429506dfe2d66184f5efc5b21220f41878 🚀

--- index.5217ab21.js
+++
@@ -1,2 +0,0 @@
-var refs={openModalBtn:document.querySelector("[data-open-reviews-form]"),closeModalBtn:document.querySelector("[data-close-reviews-form]"),backdrop:document.querySelector("[data-reviews-form]")};function toggleModal(){refs.backdrop.classList.toggle("is-hidden")}function logBackdropClick(){console.log("Це клік в бекдроп")}refs.openModalBtn.addEventListener("click",toggleModal),refs.closeModalBtn.addEventListener("click",toggleModal),refs.backdrop.addEventListener("click",logBackdropClick);
-//# sourceMappingURL=index.5217ab21.js.map
