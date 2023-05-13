da119763f14d11fd50aa3f59eff814e978eafb46
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-05-13 19:09:21 +0000

    Deploying to gh-pages from @ irashovdra/simply-chocolate@52d3d632cda411991487efdb4a069292c0db7325 🚀

---
+++ index.624ef783.js
@@ -0,0 +1,2 @@
+!function(){var e,c,r;e=document.querySelector(".burger"),c=document.querySelector(".drawer"),r=document.querySelector(".cross"),e.addEventListener("click",(function(){c.classList.add("show")})),r.addEventListener("click",(function(){c.classList.remove("show")}))}();
+//# sourceMappingURL=index.624ef783.js.map
