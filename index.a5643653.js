41d65f9b65debea1fd18776797c33d6bcb433597
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-05-19 21:40:43 +0000

    Deploying to gh-pages from @ irashovdra/simply-chocolate@5be04052d1bad5e362ca72d9cc86d12f7ba8f085 🚀

---
+++ index.a5643653.js
@@ -0,0 +1,2 @@
+new Swiper(".reviewsSwiper",{slidesPerView:1,spaceBetween:10,pagination:{el:".reviews-swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1,spaceBetween:0},768:{slidesPerView:2,spaceBetween:16},1200:{slidesPerView:3,spaceBetween:28}}});
+//# sourceMappingURL=index.a5643653.js.map
