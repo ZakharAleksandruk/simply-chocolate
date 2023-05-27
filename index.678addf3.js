bf177caea2574dc8a76fe2590a36098035e0fd4a
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-05-27 17:59:57 +0000

    Deploying to gh-pages from @ irashovdra/simply-chocolate@90b7bc8a0c74acafb6ac36f198434a554fc1c563 🚀

--- index.678addf3.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector(".burger"),c=document.querySelector(".drawer"),r=document.querySelector(".cross-btn");e.addEventListener("click",(()=>{c.classList.remove("hide")})),r.addEventListener("click",(()=>{c.classList.add("hide")}))})();
-//# sourceMappingURL=index.678addf3.js.map
